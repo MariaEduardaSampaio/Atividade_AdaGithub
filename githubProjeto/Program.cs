1c9ce7d286df214176900738588033d8ece482ca
Maria Eduarda <dudah.s3@gmail.com>
2023-12-13 16:30:24 -0300

    altera console

--- githubProjeto/Program.cs
+++ githubProjeto/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, duda!");
+            Console.WriteLine("Hello, Duda!");
         }
     }
 }
