1b216f62b7cce2996337461a2cfc369070ea642e
Suellen Seiberlick Reis <suellenr@me.local>
2023-12-13 17:06:07 -0300

    Apresentação Suellen

--- githubProjeto/Program.cs
+++ githubProjeto/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            ContribuicaoSuellen ola = new ContribuicaoSuellen();
+
+            ola.Nome = "Suellen";
+            ola.Apresentar();
         }
     }
 }
